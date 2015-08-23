f6aa53d81592160eee800a2999ac040277d1e854
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2015-08-23 15:18:59 +0100

    added terrain, AI script

---
+++ Assets/Scripts/AIScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIScript : MonoBehaviour {
+
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+
+		GoToNewPoint ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (agent.remainingDistance < 0.5f)
+			GoToNewPoint();
+	}
+
+	void GoToNewPoint () {
+		agent = GetComponent<NavMeshAgent>();
+		
+		float maxWalkDistance = 50f;
+		Vector3 direction = Random.insideUnitSphere * maxWalkDistance;
+		direction += transform.position;
+		
+		NavMeshHit hit;
+		NavMesh.SamplePosition(direction, out hit, Random.Range(0f, maxWalkDistance), 1);
+		
+		Vector3 destination = hit.position;
+		agent.SetDestination(destination);
+	
+	}
+}

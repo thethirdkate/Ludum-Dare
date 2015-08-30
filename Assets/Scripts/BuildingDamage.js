e7667e94bbc33864ccae5b43a65ebadb38e45d60
katekillick <kate.killick@10to8.com>
2015-08-30 15:38:48 +0100

    Buildings toppled count added

--- Assets/Scripts/BuildingDamage.js
+++ Assets/Scripts/BuildingDamage.js
@@ -1,9 +1,28 @@
 ﻿#pragma strict
 
+var myYPosition : float; //starting y position of the building top
+var yChangeLimit : float = 0.2; //how much the y value of the building top can change before the building is considered toppled
+
+var toppled : boolean = false;
+
+var player : GameObject;
+
 function Start () {
+
+	player = GameObject.FindGameObjectWithTag("Player");
+	myYPosition = gameObject.transform.position.y; //get the starting position
+	yChangeLimit = 0.2;
 
 }
 
 function Update () {
+
+	if (!toppled) {
+		if ((transform.position.y+yChangeLimit)<myYPosition) {
+			Debug.Log("TOPPLED");
+			toppled = true;
+			player.gameObject.GetComponent(PlayerBuildingsScore).buildingsToppled++;
+		 }
+	}
 
 }
--- Assets/Scripts/PlayerBuildingsScore.js
+++ Assets/Scripts/PlayerBuildingsScore.js
@@ -1,11 +1,15 @@
 ﻿#pragma strict
 
 var totalBuildings : int;
+var buildingsToppled : int = 0;
+
+
+public var livesText : UI.Text;
 
 function Start () {
 	totalBuildings = GameObject.FindGameObjectsWithTag("Building").length;
 }
 
 function Update () {
-
+	livesText.text = "Buildings left: " + (totalBuildings-buildingsToppled) + "/" + totalBuildings;
 }

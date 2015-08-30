1deec5aea07b90829c117d5c48f71f2174ac65ab
katekillick <kate.killick@10to8.com>
2015-08-30 22:26:16 +0100

    Experimental alternative scene added

--- Assets/Scripts/PlayerMovement.js
+++ Assets/Scripts/PlayerMovement.js
@@ -11,11 +11,16 @@ var speed : float;
 var tilt : float;
 var boundary : Boundary;
 var torque : float;
-public var rotateSpeed = 1;
+public var rotateSpeedDesktop = 0.5;
+public var rotateSpeedMobile = 1;
+
+public var mobileMode : boolean = false;
 
 var dead : boolean = false;
 
+
 function Start() {
+
 }
 
 function FixedUpdate () {
@@ -28,29 +33,41 @@ function FixedUpdate () {
 
 	if (!dead) {
 	
-	//	var verticalInput = -Input.acceleration.y;
-		var horizontalInput = Input.acceleration.x;
-		
-		if (Input.GetKey("up")) {
-			verticalInput = 1;
-		}
-		if (Input.GetKey("down")) {
-			verticalInput = -1;
-		}
-		
-		if (Input.touchCount>0) {
-			if (Input.GetTouch(0).position.x>(Screen.width/2)) { verticalInput=1; }
-			else {
-				verticalInput=-1;
+	
+		//desktop controls
+		if (!mobileMode) {
+			horizontalInput = Input.GetAxis("Horizontal")/2;
+			
+			if (Input.GetKey("up")) {
+				verticalInput = 1;
 			}
+			if (Input.GetKey("down")) {
+				verticalInput = -1;
+			}
+			rotateSpeed=rotateSpeedDesktop;
+			
 		}
+		//mobile controls
+		else {
+			horizontalInput = Input.acceleration.x;
+			if (Input.touchCount>0) {
+				//if (Input.GetTouch(0).position.x>(Screen.width/2)) { verticalInput=1; }
+				//Debug.Log(Input.GetTouch(0).position.x + " and " + Screen.width/2);
+				if (Input.GetTouch(0).position.x>0) { verticalInput=1; }
+				else {
+					verticalInput=-1;
+				}
+			}
+			rotateSpeed=rotateSpeedMobile;
+		}
+		
 		//transform.Rotate(0, Input.GetAxis ("Horizontal") * rotateSpeed, 0);
 		transform.Rotate(0, horizontalInput * rotateSpeed, 0);
 
 	    //var moveHorizontal : float = Input.GetAxis ("Horizontal");
 	     //moveVertical = Input.GetAxis ("Vertical");
 
-	     GetComponent.<Rigidbody>().AddForce( transform.forward * verticalInput *speed );
+	     GetComponent.<Rigidbody>().AddForce( transform.forward * verticalInput * speed );
 	    
 		 GetComponent.<Rigidbody>().AddTorque(horizontalInput * torque, 0, verticalInput * torque);
  	}
